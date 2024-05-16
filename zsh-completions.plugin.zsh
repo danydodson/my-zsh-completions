acfdd45dff7d232756c9aea3b1cd1fc3855c8741
danydodson <danydodson@gmail.com>
2024-05-16 00:34:15 -0500

    updates

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,7 +1,4 @@
 source "${0:h}/src/zsh-users.zsh-completions/zsh-completions.plugin.zsh"
 source "${0:h}/src/zchee.zsh-completions/zsh-completions.plugin.zsh"
-[ -x "$(command -v yarn)" ] && source "${0:h}/src/zsh-yarn-completions/zsh-yarn-completions.plugin.zsh"
-[ -x "$(command -v npm)" ] && source "${0:h}/src/custom/npm.plugin.zsh"
-[ -x "$(command -v pnpm)" ] && source "${0:h}/src/custom/pnpm.plugin.zsh"
 fpath+="${0:h}/src/custom"
 
